54cc64345fe4c48a9501cef6e9bff65b0b2f9f06
Bill Mill <bill@billmill.org>
2024-05-02 09:20:51 -0400

    feat: aggregate diffs

--- main.go
+++ main.go
@@ -11,9 +11,15 @@ import (
 	"strings"
 )
 
+type Diff struct {
+	plus  int
+	minus int
+}
+
 type File struct {
 	entry        os.DirEntry
 	status       string
+	diffSum      *Diff
 	diffStat     string
 	author       string
 	authorEmail  string
@@ -54,6 +60,12 @@ func main() {
 	gitStatus(files)
 	gitLog(files)
 	gitDiffStat(files)
+
+	// generate a diffStat graph for every file
+	for _, file := range files {
+		file.diffStat = makeDiffGraph(file)
+	}
+
 	show(dir, files, isGithub())
 }
 
@@ -83,6 +95,7 @@ func linkify(s string, github string) string {
 const (
 	BLUE  = "\033[34m"
 	GREEN = "\033[32m"
+	RED   = "\033[31m"
 	RESET = "\033[0m"
 )
 
@@ -112,6 +125,37 @@ func width(s string) int {
 	}
 
 	return n
+}
+
+// makeDiffGraph turns the total diff for a file/directory into a diff graph
+// string. Currently, this function is terrible. TODO: Copy the git logic for
+// this, and make diff width configurable maybe:
+// https://github.com/git/git/blob/d4cc1ec3/diff.c#L2862-L2874
+func makeDiffGraph(file *File) string {
+	if file.diffSum == nil {
+		return ""
+	}
+	plus := file.diffSum.plus
+	minus := file.diffSum.minus
+	if plus == 0 && minus == 0 {
+		return ""
+	}
+	if plus == 0 {
+		return fmt.Sprintf("%s----%s", RED, RESET)
+	}
+	if minus == 0 {
+		return fmt.Sprintf("%s++++%s", GREEN, RESET)
+	}
+	if plus > minus {
+		return fmt.Sprintf("%s+++%s-%s", GREEN, RED, RESET)
+	}
+	if plus == minus {
+		return fmt.Sprintf("%s++%s--%s", GREEN, RED, RESET)
+	}
+	if plus < minus {
+		return fmt.Sprintf("%s+%s---%s", GREEN, RED, RESET)
+	}
+	panic("not possible")
 }
 
 func show(dir string, files []*File, githubUrl string) {
@@ -190,7 +234,6 @@ func isGithub() string {
 // gitStatus accepts a dir and a slice of files, and adds the git status to
 // each file in place
 func gitStatus(files []*File) {
-	// Run the 'git status --porcelain' command to get the git status
 	cmd := exec.Command("git", "status", "--porcelain")
 	out, err := cmd.Output()
 	if err != nil {
@@ -242,9 +285,6 @@ func gitLog(files []*File) {
 // first returns the first part of a filepath. Given "some/file/path", it will
 // return "some". Modified from golang's built-in Split function:
 // https://github.com/golang/go/blob/c5698e315/src/internal/filepathlite/path.go#L204-L212
-//
-// Currently unused: the idea is that I'd like to use it to provide git diff
-// status for a directory, by summing up the changes contained within
 func first(path string) string {
 	i := 0
 	for i < len(path) && !os.IsPathSeparator(path[i]) {
@@ -253,36 +293,50 @@ func first(path string) string {
 	return path[:i]
 }
 
-// git diff output:
-// $ git diff --numstat --relative HEAD
-// 14      5       main.go
-// 0       0       static/fake1
-// 0       0       static/fake2
-// -       -       static/gitls.png
+// diff returns an integer for +/-, or a literal '-' for a binary file. Return
+// 0 if the file was binary; we'll just ignore it for diffStat purposes. Is
+// there anything better to do with them here?
+func diffInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+	return i
+}
+
 func gitDiffStat(files []*File) {
-	cmd := exec.Command("git", "diff", "--color", "--stat", "--stat-graph-width=4", "--relative", "HEAD")
+	cmd := exec.Command("git", "diff", "--numstat", "--relative", "HEAD")
 	output, err := cmd.Output()
 	if err != nil {
 		log.Fatalf("Diffstat error: %v", err)
 	}
 
-	diffStats := make(map[string]string)
-	lines := strings.Split(string(output), "\n")
+	diffStats := make(map[string][]Diff)
+	lines := strings.Split(strings.TrimSpace(string(output)), "\n")
 	for _, line := range lines {
-		if strings.Contains(line, "|") {
-			parts := strings.Split(line, "|")
-			path := first(strings.TrimSpace(parts[0]))
-			stats := strings.TrimSpace(parts[1])
-			// TODO: aggregate diffs. Right now we're just taking the first
-			// diff if a directory has diffs inside it, but we should be
-			// summing them up
-			diffStats[path] = stats
+		parts := strings.Split(line, "\t")
+		if len(parts) < 3 {
+			continue
 		}
+
+		plus := diffInt(parts[0])
+		minus := diffInt(parts[1])
+		path := first(strings.TrimSpace(parts[2]))
+		diffStats[path] = append(diffStats[path], Diff{plus, minus})
 	}
 
 	for _, file := range files {
+		// if the file has any diffs, sum them up. This way we aggregate a
+		// directory's diffs
 		if stats, ok := diffStats[file.entry.Name()]; ok {
-			file.diffStat = stats
+			plus := 0
+			minus := 0
+			for _, stat := range stats {
+				plus += stat.plus
+				minus += stat.minus
+			}
+
+			file.diffSum = &Diff{plus, minus}
 		}
 	}
 }
