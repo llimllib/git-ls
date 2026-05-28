155578fd3bdb88bc3a8531fe138d3f1fd3c11935
Bill Mill <bill.mill@jellyfish.co>
2026-05-28 16:16:54 -0400

    bump to 7.0.1

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 	"time"
 )
 
-const VERSION = "7.0.0"
+const VERSION = "7.0.1"
 
 // HistoryLimit is the maximum number of commits to review; we'll exit if we
 // find all files before it; if a file was more than this many commits ago we
