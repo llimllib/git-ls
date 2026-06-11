11def6f3912bc9c5f5dcb192c11ee326ba02d83c
Bill Mill <bill@billmill.org>
2026-06-11 08:22:28 -0400

    chore: move release builds to github

--- bin/release.sh
+++ bin/release.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 #
-# Use goreleaser to create a release. make sure you update VERSION in main.go
-# first
+# Tag and push a release. GoReleaser runs in GitHub Actions.
+# Make sure you update VERSION in main.go first.
 #
 # https://goreleaser.com/quick-start/
 
 version=$(grep 'VERSION =' main.go | grep -Eo "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+")
 git tag -a "v$version" -m "v$version"
 git push origin "v$version"
-goreleaser release --clean
